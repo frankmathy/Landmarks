d5ae237d8170d2152221b2b5851b7dd49e553051
Frank Mathy <frankmathy@gmail.com>
2024-11-10 21:16:41 +0100

    Implemented landmarks list

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,44 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Frank Mathy on 2024-11-10.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.black)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
